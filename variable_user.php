f29cf3ca856026d373cff720073fc12711ec5d61
Flaviarenata <flaviafitrirenata@gmail.com>
2025-02-20 12:43:05 +0700

    pratikum 1

---
+++ variable_user.php
@@ -0,0 +1,10 @@
+<?php
+// mendefinisikan variable
+$nama = " Flavia ";
+$umur = 20;
+$berat = 60.0;
+
+echo ' Nama saya ' . $nama;
+echo ' <br/>Umur :' .$umur . ' Tahun ';
+echo ' <br/>Berat :' .$berat;
+?>
